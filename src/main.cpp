b72fe19d324ecbfba4a50717e2d53ec7e66da447
MonamiYP <monami.yoshioka@gmail.com>
2023-02-23 19:23:49 +0000

    smol edit

--- src/main.cpp
+++ src/main.cpp
@@ -85,27 +85,37 @@ int main() {
     glDeleteShader(fragmentShader);
 
     float vertex_positions[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
+        0.5f,  0.5f, 0.0f,  // top right 0
+        0.5f, -0.5f, 0.0f,  // bottom right 1
+        -0.5f, -0.5f, 0.0f,  // bottom left 2
+        -0.5f,  0.5f, 0.0f   // top left 3
+    };
+    unsigned int indices[] = {
+        0, 1, 2, // triangle 1
+        1, 2, 3 // triangle 2
     };
     // Vertex buffer (VBO) is a blob of memory (in GPU), in this case it stores vertex data
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO; // Vertex buffer object, vertex attribute object, entity buffer object
     glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO); // How many buffers, id of buffer
+    glGenBuffers(1, &EBO);
+    glGenBuffers(1, &VBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO); // Select buffer (and bind it to array buffer)
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_positions), vertex_positions, GL_STATIC_DRAW); // Specify buffer data
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Need to tell OpenGL how the memory is laid out
     // Which buffer it takes data from is determined by buffer currently bound to GL_ARRAY_BUFFER
-    // Index, size, type, normalised?, space between vertices (3 vertexes of type float), offset
+    // Location, size, type, normalised?, space between vertices (3 vertexes of type float), offset
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    
+
     // Render loop
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -115,7 +125,7 @@ int main() {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window); // Double buffer for rendering, front buffer contains final output while rendering happens on the back. Then it is swapped
         glfwPollEvents(); // Checks if any events are triggered
@@ -123,6 +133,7 @@ int main() {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     glfwTerminate();
